9b7ed142728e8ba8785d9017c9151f280231fb52
nikhildarocha <niks77883@gmail.com>
2020-09-11 20:30:43 +0530

    twitter bot

---
+++ bot.py
@@ -0,0 +1,42 @@
+import tweepy
+import time
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+
+api = tweepy.API(auth)
+
+
+FILE_NAME = 'last_seen.txt'
+
+
+def read_last_seen(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
+
+
+
+def store_last_seen(FILE_NAME, last_seen_id):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
+
+
+def reply():
+    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
+    for tweet in reversed(tweets):
+        if '#randomtweet' in tweet.full_text.lower():
+            print("Replied to ID" + str(tweet.id))
+            api.update_status("@" + tweet.user.screen_name + " Hello", tweet.id)
+            store_last_seen(FILE_NAME, tweet.id)
+    
+
+
+while True:
+    reply()
+    time.sleep(2)
+    
